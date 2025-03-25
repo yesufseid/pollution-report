2ecfc53e8630f33def59b2702e71b037147f2d25
Seid Yesuf <seidyesuf750@gmail.com>
2025-03-25 17:50:42 +0300

    window

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,10 +1,18 @@
-"use client"
+"use client";
+import { useEffect, useState } from "react";
 import PollutionMap from "./component/Map";
+
 export default function Home() {
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
+
   return (
     <div>
-      <PollutionMap />
+      {isBrowser && <PollutionMap />}
     </div>
   );
-  
 }
+
