d013d332f7f907d88932fb9d37acedab269fbd5b
Seid Yesuf <seidyesuf750@gmail.com>
2025-04-04 15:26:33 +0300

    next config

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,4 +1,7 @@
-const nextConfig = {
-  output: 'export', // for Next.js 13+ with app directory
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
-module.exports = nextConfig;
+
+export default nextConfig;
