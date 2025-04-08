62b78b758001f1b13e126511c99c7a58abe91f94
Seid Yesuf <seidyesuf750@gmail.com>
2025-04-08 09:49:37 +0300

    comment

--- Frontend/middleware.ts
+++ Frontend/middleware.ts
@@ -1,9 +1,9 @@
 import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+// import { updateSession } from "@/utils/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
-}
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request);
+// }
 
 export const config = {
   matcher: [
